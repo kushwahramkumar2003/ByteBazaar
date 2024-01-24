673b68856e8d8974c0200c50d113cb4910eeec32
kushwahramkumar2003 <kushwahramkumar2003gmail.com>
2024-01-24 22:52:14 +0530

    payload completed

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,27 @@
 import express from "express";
+import {nextApp, nextHandler} from "@/next-utils";
+import {getPayloadClient} from "@/get-payload";
 
 const app = express();
 const PORT = Number(process.env.PORT) || 3000
 
 const start = async ()=>{
-    const payload = await getPayloadClient();
+    const payload = await getPayloadClient({
+        initOptions:{
+            express:app,
+            onInit:async (cms)=>{
+                cms.logger.info(`Admin URL ${cms.getAdminURL()}`)
+            }
+        }
+    });
+    app.use((req,res)=>nextHandler(req,res))
+
+    nextApp.prepare().then(()=>{
+        payload.logger.info("Next.js started")
+
+        app.listen(PORT,async ()=>{
+            payload.logger.info(`Next.js App URL : ${process.env.NEXT_PUBLIC_SERVER_URL}`)
+        })
+    })
 }
 start()
