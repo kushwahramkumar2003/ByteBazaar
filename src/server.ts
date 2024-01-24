4b5ecb60b41ace1651b98371363407abd0edd035
kushwahramkumar2003 <kushwahramkumar2003gmail.com>
2024-01-24 22:22:09 +0530

    litle bit server created

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+const PORT = Number(process.env.PORT) || 3000
+
+const start = async ()=>{
+    const payload = await getPayloadClient();
+}
+start()
